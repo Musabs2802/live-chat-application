e57a75eaef083837394cc43eb77bd07e4f2d2d4e
Musab Shaikh <musabshaikh@Musabs-MacBook-Air.local>
2024-09-17 00:23:19 +0400

    feat: message get endpoint

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -6,9 +6,32 @@ const Message = require("../models/message.model");
 
 const router = express.Router();
 
-router.post("/send", authenticate, async(req, res) => {
+router.get("/:targetId", authenticate, async(req, res) => {
+    const { targetId } = req.params
+
+    if (!targetId) {
+        return res.status(422).json({ message: "Field(s) missing" })
+    }
+
+    const user = await User.findById(req.user.id)
+    if(user) {
+        let conversation = await Conversation.findOne({ participants: { $all: [user._id, targetId] } }).populate("messages")
+        if (conversation) {
+            return res.status(200).json({ conversation })
+        }
+        else {
+            return res.status(200).json({})
+        }
+    }
+    else {
+        return res.status(401).json({ message: "Unauthorized" })
+    }
+})
+
+router.post("/send/:targetId", authenticate, async(req, res) => {
     try {
-        const { targetId, message } = req.body
+        const { targetId } = req.params
+        const { message } = req.body
 
         if(!targetId || !message) {
             return res.status(422).json({ message: "Field(s) missing" })
@@ -31,7 +54,7 @@ router.post("/send", authenticate, async(req, res) => {
 
             await Promise.all([conversation.save(), newMessage.save()])
 
-            res.status(201).json({ message: newMessage })
+            res.status(201).json({ newMessage })
         }
         else {
             return res.status(401).json({ message: "Unauthorized" })
